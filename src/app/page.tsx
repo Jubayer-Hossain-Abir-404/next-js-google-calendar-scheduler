da955a1a0ab3e2a246eed68e1e3d7ce34d9c85ce
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2024-10-20 12:27:11 +0600

    home page initiate

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+
+export default function HomePage() {
+  return (
+    <div className="text-center container my-4 mx-auto">
+      <h1 className="text-3xl mb-4">
+        Hello, World!
+      </h1>
+      <div className="flex gap-2 justify-center">
+        <Button>Sign In</Button>
+        <Button>Sign Out</Button>
+      </div>
+    </div>
+  );
+}
